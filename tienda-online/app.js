c06026e67103b87e5ca98cd17f3a4cd6eaa121fc
juanibanezo <jibanezo@unbosque.edu.co>
2026-09-23 07:55:35 -0500

    Codigo base JS para rama "feature-carrito"

---
+++ tienda-online/app.js
@@ -0,0 +1,9 @@
+// app.js - Archivo base de JavaScript
+console.log("¡Hola, mundo!");
+
+// Ejemplo de una función básica
+function saludar(nombre) {
+    return `¡Hola, ${nombre}!`;
+}
+
+console.log(saludar("Desarrollador"));
